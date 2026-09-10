b7872bcfb20b0851551be154001d0d63e090ce46
yt1 <developerpy.1@gmail.com>
2026-09-10 22:37:42 +0530

    Add multiple notes

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -17,22 +17,44 @@ class YourNotes extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> notes = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('YourNotes')),
-      body: const Center(
-        child: Text('No notes yet', style: TextStyle(fontSize: 20)),
-      ),
+
+      body: notes.isEmpty
+          ? const Center(
+              child: Text('No notes yet', style: TextStyle(fontSize: 20)),
+            )
+          : ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(notes[index]));
+              },
+            ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          final savedNote = await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const NoteScreen()),
           );
+
+          if (savedNote != null) {
+            setState(() {
+              notes.add(savedNote);
+            });
+          }
         },
         child: const Icon(Icons.add),
       ),
@@ -40,13 +62,47 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class NoteScreen extends StatelessWidget {
+class NoteScreen extends StatefulWidget {
   const NoteScreen({super.key});
+
+  @override
+  State<NoteScreen> createState() => _NoteScreenState();
+}
+
+class _NoteScreenState extends State<NoteScreen> {
+  final TextEditingController noteController = TextEditingController();
+
+  @override
+  void dispose() {
+    noteController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New Note')),
-      body: const Center(child: Text('Write your note here')),
+      appBar: AppBar(
+        title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context, noteController.text);
+            },
+            icon: const Icon(Icons.save),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: noteController,
+          decoration: const InputDecoration(
+            hintText: 'Write your note here...',
+            border: OutlineInputBorder(),
+          ),
+          maxLines: 10,
+        ),
+      ),
     );
   }
 }
